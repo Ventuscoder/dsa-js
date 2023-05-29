eaee5cb7eece830439729c32e2d9eef21d9c3576
Shivank Mitra <shivankcode@gmail.com>
2023-05-29 08:48:57 +0530

    Create stack

---
+++ data_structures/stacks/stack.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+    push(val) {
+        const newNode = new Node(val)
+        if (!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            let temp = this.first
+            this.first = newNode
+            this.first.next = temp
+        }
+        return ++this.size
+    }
+    pop() {
+        if (!this.first) return null
+        const temp = this.first
+        if (this.first === this.last) {
+            this.last = null
+        }
+        this.first = this.first.next
+        this.size--
+        return temp.val
+    }
+}
+
+const stack = new Stack()
+console.log(stack.push(1))
+console.log(stack.push(2))
+console.log(stack.push(3))
+console.log(stack.pop())
