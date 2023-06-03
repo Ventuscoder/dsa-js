fd9729d9c643aa2e043fa833538580e3b418eead
Shivank Mitra <shivankcode@gmail.com>
2023-06-03 18:10:46 +0530

    Use graph traversal algorithms

--- data_structures/graph/graph.js
+++ data_structures/graph/graph.js
@@ -21,6 +21,56 @@ class Graph {
         }
         delete this.adjacencyList[v]
     }
+    dfsRecursive(start) {
+        const visited = {}
+        const result = []
+        const adjacencyList = this.adjacencyList
+        function dfs(vertex){
+            visited[vertex] = true
+            result.push(vertex)
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) dfs(neighbor)
+            })
+        }
+        dfs(start)
+        return result
+    }
+    dfsIterative(start) {
+        const stack = [start]
+        const result = []
+        const visited = {}
+        let currentVertex
+        visited[start] = true
+        while (stack.length) {
+            currentVertex = stack.pop()
+            result.push(currentVertex)
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true
+                    stack.push(neighbor)
+                }
+            })
+        }
+        return result
+    }
+    bfs(start) {
+        const queue = [start]
+        const result = []
+        const visited = {}
+        let currentVertex
+        visited[start] = true
+        while(queue.length) {
+            currentVertex = queue.shift()
+            result.push(currentVertex)
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true
+                    queue.push(neighbor)
+                }
+            })
+        }
+        return result
+    }
 }
 
 const g = new Graph()
@@ -29,6 +79,8 @@ g.addVertex('LA')
 g.addVertex('SAC')
 g.addVertex('DAL')
 g.addVertex('PHX')
+g.addVertex('SEA')
+g.addVertex('DEN')
 g.addEdge('SF', 'SAC')
 g.addEdge('LA', 'SAC')
 g.addEdge('SF', 'LA')
@@ -36,5 +88,8 @@ g.addEdge('SAC', 'PHX')
 g.addEdge('LA', 'PHX')
 g.addEdge('LA', 'DAL')
 g.addEdge('PHX', 'DAL')
-g.removeVertex('SAC')
-console.log(g.adjacencyList)
+g.addEdge('SF', 'SEA')
+g.addEdge('PHX', 'DEN')
+console.log('Depth First Search Recursive:\n', g.dfsRecursive('SF'))
+console.log('Depth First Search Iterative:\n', g.dfsIterative('SF'))
+console.log('Breadth First Search:\n', g.bfs('SF'))
